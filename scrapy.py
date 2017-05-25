ee40a4991194660b437b6a4e79d294736b3e1a4c
zluis1992 <zluissigloxx@hotmail.com>
2017-05-25 13:57:15 -0500

    comentarios en toda la lista de videos
    se da clic en todos los videos y se escriben en los comentarios de cada uno.

--- scrapy.py
+++ scrapy.py
@@ -45,9 +45,10 @@ wait = WebDriverWait(browser, 1000)
 element = wait.until(EC.element_to_be_clickable((By.ID, 'masthead-search-term')))
 browser.find_element_by_id('masthead-search-term').click()
 browser.find_element_by_id('masthead-search-term').clear()
-browser.find_element_by_id('masthead-search-term').send_keys('Daniel Samper')
+browser.find_element_by_id('masthead-search-term').send_keys('shakiraVEVO')
 browser.find_element_by_id('masthead-search-term').send_keys(u'\ue007')
 
+#declaramos estas listas, que es donde se guardaran los videos.
 resultado = []
 resultado2 = []
 
@@ -66,8 +67,15 @@ element = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="channel-nav
 
 
 #aca le doy clic al boton subscribirse, se debe ahora para pruebas anular la subscripcion antes de volver a correr el script
-browser.find_element_by_css_selector('.subscribe-label').click()
-time.sleep(1)#segundos
+
+try:
+    browser.find_element_by_css_selector('.subscribe-label').click()
+except ValueError:
+    print('entre al boton que esta ya subscrito')
+
+
+#browser.find_element_by_css_selector('.subscribe-label').click()
+time.sleep(1) #segundos
 browser.find_element_by_xpath('//*[@id="channel-navigation-menu"]/li[2]/a/span').click()
 
 wait = WebDriverWait(browser, 1000)
@@ -80,23 +88,31 @@ for x in xrange(1, 5):
 	time.sleep(0.5)
 
 resultado = browser.find_elements_by_css_selector('.yt-uix-sessionlink.yt-uix-tile-link.spf-link.yt-ui-ellipsis.yt-ui-ellipsis-2')
-for p in resultado :
-	# print p.get_attribute('title')
 
-	browser.get(p.get_attribute('href'))
+#este para se hizo para poder guardar todas los href de todos los videos.
+for x in resultado:
+	resultado2.append(x.get_attribute('href'))
+
+for p in resultado2 :
+	#print p.get_attribute('title')
+
+	#browser.get(p.get_attribute('href'))
+	browser.get(p)
 	wait = WebDriverWait(browser, 1000)
 	element = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.watch-view-count')))
 
-	for x in xrange(1, 2):
+	for x in xrange(1, 3):
 		browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-		time.sleep(0.5)
+		time.sleep(0.5) #espera un tiempo que sino no carga el cuadro de los comentarios
+		browser.execute_script("window.scrollTo(0, 0);")
 
-	# Se seleccciona el cuadro de comentarios, se escribe el comentario y se envía
+	# Se seleccciona el cuadro de comentarios, se escribe el comentario y se envia
 	browser.find_element_by_css_selector('.comment-simplebox-renderer-collapsed-content').click()
 	browser.find_element_by_css_selector('.comment-simplebox-text').send_keys('Buen video')
 	browser.find_element_by_css_selector('.yt-uix-button.yt-uix-button-size-default.yt-uix-button-primary.yt-uix-button-empty.comment-simplebox-submit.yt-uix-sessionlink').click()
-
-	time.sleep(10)
+	print browser.find_element_by_id('eow-title').get_attribute('title')
+	time.sleep(10) #demora 10 segundos para pasar al proximo video
+	
 
 #aca simplemente tomamos un elemento del que tenemos y lo volvemos a mandar a buscarlo
 #wait = WebDriverWait(browser, 10000)
